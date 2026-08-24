52a82935d3b7daaf3a1f8d726259b762fb44dbca
JOHN MARC LOZAÑES <115021344+Jmlozanes@users.noreply.github.com>
2026-08-24 11:28:57 +0800

    Implement hero and enemy attack mechanics

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
+let heroHP = 100;
+let enemyHP = 100;
 
+
+function attack(){
+
+    // Hero attack
+    let damage = Math.floor(Math.random() * 20) + 1;
+
+    enemyHP -= damage;
+
+
+    document.getElementById("log").innerHTML =
+    "Hero attacked Goblin! Damage: " + damage;
+
+
+    updateHealth();
+
+
+    // Check kung patay ang enemy
+    if(enemyHP <= 0){
+
+        enemyHP = 0;
+
+        document.getElementById("log").innerHTML =
+        "🎉 You defeated the Goblin!";
+
+        return;
+    }
+
+
+
+    // Enemy counter attack
+
+    let enemyDamage = Math.floor(Math.random() * 15) + 1;
+
+    heroHP -= enemyDamage;
+
+
+    document.getElementById("log").innerHTML +=
+    "<br>Goblin attacked you! Damage: " + enemyDamage;
+
+
+
+    updateHealth();
+
+
+
+    if(heroHP <= 0){
+
+        heroHP = 0;
+
+        document.getElementById("log").innerHTML =
+        "💀 You were defeated!";
+
+    }
+
+
+}
+
+
+
+function updateHealth(){
+
+
+    document.getElementById("enemyHP").style.width =
+    enemyHP + "%";
+
+
+    document.getElementById("heroHP").style.width =
+    heroHP + "%";
+
+
+    document.getElementById("enemyHealthText").innerHTML =
+    "HP: " + enemyHP;
+
+
+    document.getElementById("heroHealthText").innerHTML =
+    "HP: " + heroHP;
+
+
+}
